50d1d11a8d1257971365ecb6edffc71efa0f4cc9
vinnyegon90 <vinnyegon90@gmail.com>
2024-04-05 09:54:16 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -18,6 +18,11 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
+
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee',  cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
@@ -30,6 +35,9 @@ class Onboarding(db.Model):
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    employee = db.relationship('Employee', back_populates='onboarding')
+
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
@@ -40,6 +48,9 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    employee = db.relationship('Employee', back_populates="reviews")
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
